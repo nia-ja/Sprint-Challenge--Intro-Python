83504a7211927b7046c91b174104dfe11fa7a056
Kasia Bondarava <kasia.bondarava@gmail.com>
2020-02-21 19:19:40 -0500

    Add solution for oop1 challenge. Tests are passing

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+## base class for all other classes
+class Vehicle:
+    pass
+
+## base class for Car and Motorcycle
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+## Base class for Starship and Airplane
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
